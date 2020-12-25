0e2f2ca613902c395d9b151944fca5761b4b9edf
A Hanson <hansonanna1988@gmail.com>
2020-12-25 13:30:59 -0700

    add Objective 2, labs 1-3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+    public static void main(String[] args) {
+        String myVariable = "All I want for Christmas";
+        System.out.println(myVariable);
+        myVariable = "Is you.";
+        System.out.println(myVariable);
+    }
+}
